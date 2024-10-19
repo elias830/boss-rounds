f7403e20a6e7f2ac8448d0476fb4d2935ec01a08
elias830 <82284057+elias830@users.noreply.github.com>
2024-10-19 14:48:19 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace BossRounds;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "38.0";
+    public const string WorksOnVersion = "45.0";
     public const string Version = "1.0.10";
     public const string Name = "Boss Rounds";
 
